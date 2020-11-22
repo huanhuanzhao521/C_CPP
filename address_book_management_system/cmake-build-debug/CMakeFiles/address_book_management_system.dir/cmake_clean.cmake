4e31dd9cdaf8d8061180f5340db9f62cdf372524
hashiqi-233 <zheng_wei2333@163.com>
2020-11-22 21:32:31 +0800

    通讯录管理系统联系

---
+++ address_book_management_system/cmake-build-debug/CMakeFiles/address_book_management_system.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/address_book_management_system.dir/main.cpp.o"
+  "address_book_management_system.pdb"
+  "address_book_management_system.exe"
+  "libaddress_book_management_system.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/address_book_management_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- address_book_management_system/main.cpp
+++ address_book_management_system/main.cpp
@@ -1,4 +1,10 @@
 //
 // Created by Administrator on 2020/11/22 0022.
 //
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
 
