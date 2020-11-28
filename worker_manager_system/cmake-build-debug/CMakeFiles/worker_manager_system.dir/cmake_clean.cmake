669fa278ae4119e1c93a296f3506d5aa23c6a307
hashiqi-233 <zheng_wei2333@163.com>
2020-11-28 16:02:53 +0800

    员工管理系统

---
+++ worker_manager_system/cmake-build-debug/CMakeFiles/worker_manager_system.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/worker_manager_system.dir/main.cpp.o"
+  "worker_manager_system.pdb"
+  "worker_manager_system.exe"
+  "libworker_manager_system.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/worker_manager_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
